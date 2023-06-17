2c3aeb49ce5e4d32f6d5850c69926a7898806be7
Jaro <saichi5@gmail.com>
2023-06-17 16:07:15 +0900

    account3

--- src/components/organisms/editor-form.tsx
+++ src/components/organisms/editor-form.tsx
@@ -9,6 +9,8 @@ import { saveOldUser } from '@/utils/data-fetch';
 import { useUser } from '@/components/user-context';
 import Image from "next/image";
 import moment from 'moment';
+import Dropzone from 'react-dropzone';
+import { uploadUserImage } from '@/utils/file-loading';
 
 export default function EditorForm (){
 
@@ -36,6 +38,53 @@ export default function EditorForm (){
     console.log('akira03: ' + displayName)
   }, [] );
 
+  // ブラウザ表示用の paths
+  const [previewCoverPath, setPreviewCoverPath] = useState<string>();
+  const [previewAvatorPath, setPreviewAvatorPath] = useState<string>();
+
+  // upload用の files
+  const [cover, setCover] = useState<File>();
+  const [avator, setAvator] = useState<File>();
+
+  //********************* 
+  /** 選択された画像を処理 */
+  //*********************
+  // avator
+  const onDropAvator = (acceptedFiles: File[]) => {
+    // 引数で受け取れる値は、File型の配列なので upload用のstateへsetする
+    setAvator(acceptedFiles[0]);
+    // ブラウザで画像を表示させるための、一時的なURLをメモリに生成する
+    // @see https://developer.mozilla.org/ja/docs/Web/API/URL/createObjectURL
+    const dataUrl = URL.createObjectURL(acceptedFiles[0]);
+    // createObjectURLで生成された、ブラウザ表示用のURLをstateへsetする
+    setPreviewAvatorPath(dataUrl);
+  };
+
+  // cover picture
+  const onDropCover = (acceptedFiles: File[]) => {
+    // 引数で受け取れる値は、File型の配列なので upload用のstateへsetする
+    setCover(acceptedFiles[0]);
+    // ブラウザで画像を表示させるための、一時的なURLをメモリに生成する
+    // @see https://developer.mozilla.org/ja/docs/Web/API/URL/createObjectURL
+    const dataUrl = URL.createObjectURL(acceptedFiles[0]);
+    // createObjectURLで生成された、ブラウザ表示用のURLをstateへsetする
+    setPreviewCoverPath(dataUrl);
+  };
+
+  const buildFormData = (file?: File) => {
+    if (!file) {
+      return new FormData();
+    }
+    // DB へアップロードするために、FormData へ append する
+    // @see https://developer.mozilla.org/ja/docs/Web/API/FormData/Using_FormData_Objects
+    const formData = new FormData();
+    // append の第一引数はバックエンドと合わせる
+    formData.append('image', file, file.name);
+
+    return formData;
+  };
+
+
   const handleSubmit = async ( e: FormEvent<HTMLFormElement> ) => {
     e.preventDefault();
 
@@ -43,6 +92,13 @@ export default function EditorForm (){
     const dateString = moment().format('YYYY-MM-DD');
 
     try {
+      let formData = buildFormData(cover);
+      let res = await uploadUserImage( formData );
+      setCoverImageUrl( '/pictures/users/' + res ?? '');
+      formData = buildFormData(avator);
+      res = await uploadUserImage( formData );
+      setProfileImageUrl( '/pictures/users/' + res ?? '');
+
         const oldUser: User = {
           id: user.id,
           displayName: displayName as string,
@@ -125,14 +181,22 @@ export default function EditorForm (){
               <label htmlFor="photo" className="block text-sm font-medium leading-6 text-gray-900">
                 アバター
               </label>
-              <div className="mt-2 flex items-center gap-x-3">
-                <UserCircleIcon className="h-12 w-12 text-gray-300" aria-hidden="true" />
-                <button className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-                  <input
-                    type="file"
-                    onChange={onChangeImage} />
-                  変更
-                </button>
+              <div  className="mt-2 flex items-center gap-x-3">
+              {previewAvatorPath ? (
+                    <Image src={previewAvatorPath} alt='' height={0} width={0} className="h-12 w-12 rounded-full text-gray-300" aria-hidden="true" />
+              ) : (
+                  <UserCircleIcon className="h-12 w-12 text-gray-300" aria-hidden="true" />
+              )}
+                <Dropzone onDrop={onDropAvator}>
+                  {({getRootProps, getInputProps}) => (
+                    <div {...getRootProps({ className: `dropzone "cursor-pointer rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-500"` })}>
+                      <input {...getInputProps()} />
+                      <p className="relative cursor-pointer rounded-md bg-white focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2">
+                        変更
+                      </p>
+                    </div>
+                  )}
+                </Dropzone>
               </div>
             </div>
             {/* coverImageUrl  */}
@@ -140,22 +204,31 @@ export default function EditorForm (){
               <label htmlFor="cover-photo" className="block text-sm font-medium leading-6 text-gray-900">
                 カバー画像
               </label>
-              <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
-                <div className="text-center">
-                  <PhotoIcon className="mx-auto h-12 w-12 text-gray-300" aria-hidden="true" />
-                  <div className="mt-4 flex text-sm leading-6 text-gray-600">
-                    <label
-                      htmlFor="file-upload"
-                      className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
-                    >
-                      <span>アップロード</span>
-                      <input id="file-upload" name="file-upload" type="file" className="sr-only" />
-                    </label>
-                    <p className="pl-1">or drag and drop</p>
+              <Dropzone onDrop={onDropCover}>
+                {({getRootProps, getInputProps}) => (
+                  <div {...getRootProps({ className: `dropzone "mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10"` })}>
+                    <div className="text-center">
+                      <PhotoIcon className="mx-auto h-12 w-12 text-gray-300" aria-hidden="true" />
+                      <div className="mt-4 flex text-sm leading-6 text-gray-600">
+                        <label
+                          htmlFor="file-upload"
+                          className="relative cursor-pointer rounded-md bg-white text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                        >
+                          <span>アップロード</span>
+                          <input {...getInputProps()} />
+                          <div className='text-gray-600'>
+                            <p className="pl-1">or</p>
+                            <p className="pl-1">画像ファイルをドラッグ&ドロップ</p>
+                          </div>
+                        </label>
+                      </div>
+                    </div>
                   </div>
-                  <p className="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
-                </div>
-              </div>
+                )}
+              </Dropzone>
+              {previewCoverPath &&
+                <Image src={previewCoverPath} alt='' height={150} width={174} />
+              }
             </div>
           </div>
         </div>
