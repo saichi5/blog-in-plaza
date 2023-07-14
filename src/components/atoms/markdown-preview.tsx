60649ce0eb70f3d5d7a7d6cadcdbebf5f613a9a8
jaro <saichi5@gmail.com>
2023-07-14 12:12:06 +0900

    editor2

--- src/components/atoms/markdown-preview.tsx
+++ src/components/atoms/markdown-preview.tsx
@@ -1,16 +1,16 @@
 import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
 import rehypeRaw from 'rehype-raw';
 import remarkGfm from "remark-gfm";
+// import { Scrollbar } from '@/components/scrollbar';
 import '@/app/markdown/styles.css'
 
 export default function MarkdownPreview(props: {contents: string}){
   const contents = props.contents
   return (
-    <p>
-    <ReactMarkdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]}>
-      {contents}
-    </ReactMarkdown>
-  </p>
-
+    <div>
+      <ReactMarkdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]}>
+        {contents}     
+      </ReactMarkdown>
+    </div>
   )
 }
